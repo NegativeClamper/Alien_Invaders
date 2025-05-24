81cfcc852c09b8ea569cb7253b869941f2e35c72
MURDER GOOSE <103416388+NegativeClamper@users.noreply.github.com>
2025-05-24 18:43:38 +0530

    settings for fleet

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -8,6 +8,11 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
         self.ship_speed = 1.5
+        #Alien settings
+        self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        #fleet_direction of 1 represents right; -1 represents left.
+        self.fleet_direction = 1
 
         #Bullet settings
         self.bullet_speed = 1.0
@@ -15,3 +20,5 @@ class Settings:
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+
+        
