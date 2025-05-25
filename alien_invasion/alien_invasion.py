d425dee4898f329736bdb8ad049f3ccc5283db46
MURDER GOOSE <103416388+NegativeClamper@users.noreply.github.com>
2025-05-25 15:02:03 +0530

    game_stats

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,8 +1,10 @@
 import sys
+from time import sleep
 
 import pygame
 
 from settings import Settings
+from game_stats import GameStats
 from ship import Ship
 from bullet import Bullet
 from alien import Alien
@@ -20,6 +22,9 @@ class AlienInvasion:
 
         #self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+
+        #Create an instance to store game statistics.
+        self.stats = GameStats(self)
 
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
@@ -83,10 +88,45 @@ class AlienInvasion:
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
 
+        self._check_bullet_alien_collisions()
+    
+    def _check_bullet_alien_collisions(self):
+        """Respond to bullet-alien collisions."""
+        #Remove any bullets and aliens that have collided.
+
+        #Check for any bullets that have hit aliens.
+        #If so, get rif of the bullet and the alien.
+        collisions = pygame.sprite.groupcollide(self.bullets, self.aliens, True, True)
+
+        if not self.aliens:
+            #Destroy existing bullets and create new fleet.
+            self.bullets.empty()
+            self._create_fleet()
+
     def _update_aliens(self):
         """Check if the fleet is at an edge, then update the positions of all alliens in the fleet."""
         self._check_fleet_edges()
         self.aliens.update()
+
+        #Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!! we have been compromised.")
+
+    def _ship_hit(self):
+        """Respond to the ship being hit by an alien."""
+        #Decrement ships_left.
+        self.stats.ships_left -= 1
+        
+        #Get red of anu remaining aliens andd bullets.
+        self.aliens.empty()
+        self.bullets.empty()
+
+        #Create a new fleet and center the ship.
+        self._create_fleet()
+        self.ship.center_ship()
+
+        #Pause.
+        sleep(0.5)
 
     def _create_fleet(self):
         """Create the fleet of aliens."""
--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -7,7 +7,11 @@ class Settings:
         self.screen_width = 800
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
+        
+        #Ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
+        
         #Alien settings
         self.alien_speed = 1.0
         self.fleet_drop_speed = 10
@@ -15,10 +19,11 @@ class Settings:
         self.fleet_direction = 1
 
         #Bullet settings
-        self.bullet_speed = 1.0
+        self.bullet_speed = 1.5
         self.bullet_width = 3
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
+    
 
         
